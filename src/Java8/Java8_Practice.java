91aa9fa93062f9329e7f2c1f24b88d267bebde5d
Suman Maity <sommaity25d@gmail.com>
2026-01-14 17:00:12 +0530

    second commit

--- src/Java8/Java8_Practice.java
+++ src/Java8/Java8_Practice.java
@@ -10,6 +10,8 @@ public class Java8_Practice {
         number.add(2);
         number.add(3);
         number.add(4);
+        number.add(5);
+        number.add(6);
 
         number.stream().filter(x->x%2==0).forEach(System.out::println);
     }
